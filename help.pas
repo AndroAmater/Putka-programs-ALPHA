9a4c53fc7e792248abaab5e52a638a82c018090e
AndroAmater <molekula2@gmail.com>
2013-12-05 21:26:12 +0100

    Added ':src' Command to help

--- help.pas
+++ help.pas
@@ -10,6 +10,8 @@ implementation
     writeln(':l - List currently implemented programs');
     writeln(':r - Run a program. Enter ":r" then enter');
     writeln('     program number shown by list command');
+    writeln(':r - Show source code. Enter ":src" then enter');
+    writeln('     program number shown by list command');
     writeln(':c - Clear screen');
     writeln(':q - Quit program');
     writeln('------------------(>0_0>)-----------------');
